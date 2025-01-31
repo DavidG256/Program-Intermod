a0fb6523998f7d84ea39bbed8bdc540d318ad4aa
DavidG256 <davgarant@alu.edu.gva.es>
2025-01-31 10:27:32 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Vendedor.IDVendedor vendedor = new Vendedor();
+        Vendedor.esJefe esJefe = new Vendedor();
+        Vendedor.salario salario = new Vendedor();
+        Vendedor.jornada jornada = new Vendedor();
+        
+    }
+}
---
+++ Vendedor.java
@@ -0,0 +1,69 @@
+public class Vendedor {
+    protected String IDVendedor;
+    protected boolean esJefe;
+    protected double salario;
+    protected double jornada;
+
+
+    public Vendedor(String IDVendedor) {
+        this.IDVendedor = IDVendedor;
+
+    }
+
+    public String getIDVendedor() {
+        return IDVendedor;
+    }
+
+    public void setIDVendedor(String IDVendedor) {
+        this.IDVendedor = IDVendedor;
+    }
+
+
+    public Vendedor(boolean esJefe) {
+        this.esJefe = esJefe;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public Vendedor(double jornada) {
+        this.jornada = jornada;
+    }
+
+    public double getJornada() {
+        return jornada;
+    }
+
+    public void setJornada(double jornada) {
+        this.jornada = jornada;
+    }
+
+    public class IDVendedor {
+
+    }
+
+
+
+    public class esJefe {
+
+
+    }
+
+    public class salario {
+
+    }
+
+
+    public class jornada {
+
+    }
+
+
+
+
+}
